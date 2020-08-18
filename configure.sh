9ac6a9f6835ea501e19167bd57bd838c7f95c040
laintuv00 <laintuv00@gmail.com>
2020-08-18 16:38:49 +0700

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -14,184 +14,70 @@ rm -rf /tmp/v2ray
 install -d /usr/local/etc/v2ray
 cat << EOF > /usr/local/etc/v2ray/config.json
 {
-	"log": {
-		"access": "/var/log/v2ray/access.log",
-		"error": "/var/log/v2ray/error.log",
-		"loglevel": "warning"
-	},
-	"inbounds": [
-		{
-			"port": $PORT,
-			"protocol": "vmess",
-			"settings": {
-				"clients": [
-					{
-						"id": "$UUID",
-						"level": 1,
-						"alterId": 233
-					}
-				]
-			},
-			"streamSettings": {
-				"network": "tcp",
-				"tcpSettings": {
-					"header": {
-						"type": "http",
-						"response": {
-							"version": "1.1",
-							"status": "200",
-							"reason": "OK",
-							"headers": {
-								"Content-encoding": [
-									"gzip"
-								],
-								"Content-Type": [
-									"text/html; charset=utf-8"
-								],
-								"Cache-Control": [
-									"no-cache"
-								],
-								"Vary": [
-									"Accept-Encoding"
-								],
-								"X-Frame-Options": [
-									"deny"
-								],
-								"X-XSS-Protection": [
-									"1; mode=block"
-								],
-								"X-content-type-options": [
-									"nosniff"
-								]
-							}
-						}
-					}
-				}
-			},
-			"sniffing": {
-				"enabled": true,
-				"destOverride": [
-					"http",
-					"tls"
-				]
-			}
-		}
-		//include_ss
-		//include_socks
-		//include_mtproto
-		//include_in_config
-		//
-	],
-	"outbounds": [
-		{
-			"protocol": "freedom",
-			"settings": {
-				"domainStrategy": "UseIP"
-			},
-			"tag": "direct"
-		},
-		{
-			"protocol": "blackhole",
-			"settings": {},
-			"tag": "blocked"
-        },
-		{
-			"protocol": "mtproto",
-			"settings": {},
-			"tag": "tg-out"
-		}
-		//include_out_config
-		//
-	],
-	"dns": {
-		"servers": [
-			"https+local://cloudflare-dns.com/dns-query",
-			"1.1.1.1",
-			"1.0.0.1",
-			"8.8.8.8",
-			"8.8.4.4",
-			"localhost"
-		]
-	},
-	"routing": {
-		"domainStrategy": "IPOnDemand",
-		"rules": [
-			{
-				"type": "field",
-				"ip": [
-					"0.0.0.0/8",
-					"10.0.0.0/8",
-					"100.64.0.0/10",
-					"127.0.0.0/8",
-					"169.254.0.0/16",
-					"172.16.0.0/12",
-					"192.0.0.0/24",
-					"192.0.2.0/24",
-					"192.168.0.0/16",
-					"198.18.0.0/15",
-					"198.51.100.0/24",
-					"203.0.113.0/24",
-					"::1/128",
-					"fc00::/7",
-					"fe80::/10"
-				],
-				"outboundTag": "blocked"
-			},
-			{
-				"type": "field",
-				"inboundTag": ["tg-in"],
-				"outboundTag": "tg-out"
-			}
-			,
-			{
-				"type": "field",
-				"domain": [
-					"domain:epochtimes.com",
-					"domain:epochtimes.com.tw",
-					"domain:epochtimes.fr",
-					"domain:epochtimes.de",
-					"domain:epochtimes.jp",
-					"domain:epochtimes.ru",
-					"domain:epochtimes.co.il",
-					"domain:epochtimes.co.kr",
-					"domain:epochtimes-romania.com",
-					"domain:erabaru.net",
-					"domain:lagranepoca.com",
-					"domain:theepochtimes.com",
-					"domain:ntdtv.com",
-					"domain:ntd.tv",
-					"domain:ntdtv-dc.com",
-					"domain:ntdtv.com.tw",
-					"domain:minghui.org",
-					"domain:renminbao.com",
-					"domain:dafahao.com",
-					"domain:dongtaiwang.com",
-					"domain:falundafa.org",
-					"domain:wujieliulan.com",
-					"domain:ninecommentaries.com",
-					"domain:shenyun.com"
-				],
-				"outboundTag": "blocked"
-			}			,
-                {
-                    "type": "field",
-                    "protocol": [
-                        "bittorrent"
-                    ],
-                    "outboundTag": "blocked"
-                }
-			//include_ban_ad
-			//include_rules
-			//
-		]
-	},
-	"transport": {
-		"kcpSettings": {
-            "uplinkCapacity": 100,
-            "downlinkCapacity": 100,
-            "congestion": true
+  "log" : {
+    "access": "/var/log/v2ray/access.log",
+    "error": "/var/log/v2ray/error.log",
+    "loglevel": "warning"
+  },
+  "inbounds": [
+    {
+      "port": $PORT,
+      "protocol": "vmess",
+      "settings": {
+        "clients": [
+          {
+            "id": "$UUID",
+            "level": 1,
+            "alterId": 64
+          }
+        ]
+      },
+      "streamSettings": {
+        "network": "tcp",
+        "tcpSettings": {
+          "header": {
+            "type": "http",
+            "response": {
+              "version": "1.1",
+              "status": "200",
+              "reason": "OK",
+              "headers": {
+                "Content-Type": ["application/octet-stream", "application/x-msdownload", "text/html", "application/x-shockwave-flash"],
+                "Transfer-Encoding": ["chunked"],
+                "Connection": ["keep-alive"],
+                "Pragma": "no-cache"
+              }
+            }
+          }
         }
-	}
+      }
+    }
+  ],
+  "outbounds": [
+    {
+      "protocol": "freedom",
+      "settings": {}
+    },
+    {
+      "protocol": "blackhole",
+      "settings": {},
+      "tag": "blocked"
+    }
+  ],
+  "routing": {
+    "strategy": "rules",
+    "settings": {
+      "rules": [
+        {
+          "type": "field",
+          "ip": [
+            "geoip:private"
+          ],
+          "outboundTag": "blocked"
+        }
+      ]
+    }
+  }
 }
 EOF
 
